ce30399417c451e06c957ae9e2ebc5a6e8051719
abhi <75876839+abhiraj-k-r@users.noreply.github.com>
2021-06-11 19:44:36 +0530

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,115 @@
+let resumeData = {
+    "imagebaseurl":"https:example.com",
+    "name": "Abhiraj K R",
+    "role": "Frontend Developer and Network Engineer",
+    "linkedinId":"https://www.linkedin.com/in/abhi1223334444raj/",
+    "skypeid": "demo skype id",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical blogs and make DIY projects using Arduino, during my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/abhi1223334444raj/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at NIT Agartala and pursuing my B.Tech. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://example.com",
+    "education":[
+      {
+        "UniversityName":"NIT Agartala",
+        "specialization":"B.Tech",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2022",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"JNV Kottayam",
+        "specialization":"Science",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
+        "Achievements":"Leader"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"FOSSEE IIT Bombay",
+        "specialization":"Web Developer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Clear Exam",
+        "specialization":"Front End Developer",
+        "MonthOfLeaving":"Sep",
+        "YearOfLeaving":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Arduino"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Raspberry Pi"
+      },
+      {
+        "skillname":"C"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"ReactJS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"demo project1",
+        "imgurl":"images/portfolio/project1.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"demo project2",
+        "imgurl":"images/portfolio/project2.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"demo project3",  
+        "imgurl":"images/portfolio/project3.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"demo project4",
+        "imgurl":"images/portfolio/project4.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial1",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial2",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
